f16657a14f30f2ed1b2fbe619acaab38cd76570a
Alexei Rusu <rusualexei1995@gmail.com>
2023-02-25 13:47:35 -0500

    added optional field to model

--- inventory/models.py
+++ inventory/models.py
@@ -6,7 +6,7 @@ class Inventory(models.Model):
     name = models.CharField(max_length=100)
     quantity = models.IntegerField()
     department = models.CharField(max_length=100)
-    image = CloudinaryField('image')
+    image = CloudinaryField('image',blank=True, null=True)
     details = models.CharField(max_length=250)
     location = models.CharField(max_length=20)
     
