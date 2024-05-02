ade05ac081bb341c7e7e08c8dcf72d9c20ae317f
Alexei Rusu <rusualexei1995@gmail.com>
2024-05-01 20:26:11 -0400

    bf

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ set -o errexit
 
 ## Install dependencies
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+# python3.9 manage.py collectstatic
 
 ## Run migrations in case any migrations hadn't been run yet
-python manage.py migrate
+# python manage.py migrate
